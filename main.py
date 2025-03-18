f20422ed641417496974d83811135eafca400ec8
= <=>
2025-03-17 21:20:16 -0300

    add the french word text to the card on click

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ to_learn = data.to_dict(orient="records")
 
 def next_card():
     current_card = random.choice(to_learn)
-    print(current_card["French"])
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=current_card["French"])
 
 window = Tk()
 window.title("IWontForget")
@@ -18,8 +19,8 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front_img)
-canvas.create_text(400, 150, text="Title", font=("Arial", 40, "italic"))
-canvas.create_text(400, 263, text="word", font=("Arial", 60, "bold"))
+card_title = canvas.create_text(400, 150, text="", font=("Arial", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Arial", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
@@ -31,5 +32,6 @@ check_image = PhotoImage(file="images/right.png")
 know_button = Button(image=check_image, highlightthickness=0, border=0, command=next_card)
 know_button.grid(row=1, column=1)
 
+next_card()
 
 window.mainloop()
